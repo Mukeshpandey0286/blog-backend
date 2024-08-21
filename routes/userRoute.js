55c1b9431eb01bf3ffa026f324470d72d425dd12
Mukesh <121652157+Mukeshpandey0286@users.noreply.github.com>
2024-08-21 16:33:04 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,12 @@
 const {Router} = require("express");
-const { signUp, signIn } = require("../controller/userController");
+const { signUp, signIn, getProfile, updateProfile } = require("../controller/userController");
+const { checkForAuthentication } = require("../middlewares/auth");
 
 const router = Router();
 
 router.post("/signup", signUp)
 .post("/signin", signIn)
+.get("/profile",checkForAuthentication, getProfile)
+.patch("/profile-update",checkForAuthentication, updateProfile)
 
 module.exports = router;
