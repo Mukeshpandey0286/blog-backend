5f8a6941a23aae691ed9c5d8f2551a787158f61b
Mukesh <121652157+Mukeshpandey0286@users.noreply.github.com>
2024-08-22 16:24:01 +0530

    Add files via upload

---
+++ services/cloudinary.js
@@ -0,0 +1,21 @@
+const {v2} = require("cloudinary");
+
+const uploadOnCloudnary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      return null;
+    }
+
+    const result = await cloudinary.uploader.upload(localFilePath, {
+      folder: "lensOpticals",
+      resource_type: "auto",
+    });
+    // console.log("File Uploaded on cloudiinary : ", result.url);
+
+    return result;
+  } catch (error) {
+    return null;
+  }
+};
+
+exports.module =  { uploadOnCloudnary };
