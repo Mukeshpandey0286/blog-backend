d75a111b0c4d311ea401e30b305fa3744af3b96a
Mukesh <121652157+Mukeshpandey0286@users.noreply.github.com>
2024-08-23 23:04:48 +0530

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -2,12 +2,15 @@ const { Router } = require("express");
 const { addBlogs, allBlogs, viewMoreAboutBlog } = require("../controller/blogController");
 const { checkForAuthentication, restrictToUser } = require("../middlewares/auth");
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' }); 
+
 const router = Router();
 
 router.get("/", checkForAuthentication, allBlogs);
 
 router.get("/:id", viewMoreAboutBlog);
 
-router.post("/add-new", checkForAuthentication, addBlogs);
+router.post("/add-new", checkForAuthentication, upload.single('coverImage'), addBlogs);
 
 module.exports = router;
